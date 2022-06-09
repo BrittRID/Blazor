744cd8767a56df3bbc66727a3e4c8222c27e4e7c
Brittany Ridley <britt.ridley@outlook.com>
2022-06-08 20:00:17 -0500

    AddedService

---
+++ BlazorEcommerce/Client/Services/ProductService/IProducrService.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorEcommerce.Client.Services.ProductService
+{
+    public interface IProducrService
+    {
+        List<Product> Products { get; set; }
+        Task GetProducts();
+    }
+}
